33e6a690cc854e4a129d5968a3d8bd0154aa168e
Jason J. W <71363607+digaji@users.noreply.github.com>
2022-03-27 23:06:09 +0700

    Implemented function, class, var parsing

--- input1.php
+++ input1.php
@@ -1,3 +1,8 @@
 <?php
-;;
+class Hello {
+    // this is a comment
+    function abc() {
+        $hello=5 / 2;
+    }
+}
 ?>
--- lexer.py
+++ lexer.py
@@ -1,29 +1,29 @@
-import re
+import re, string
 
-#TODO
-# angka
-# Variable identifier
+# TODO
 # String literal
+# single line comment (if read)
+# multi line comment (if read)
 
 # CONSTANTS
 TOKENS = {
-    "=": "assignment",
+    "=": "assign",
     "+": "addition_sign",
     "-": "minus_sign",
     "*": "multiply",
     "/": "divide",
     ";": "semicolon",
-    "$": "php_var",
-    "function": "php_function",
-    "class": "php_class",
-    "echo": "php_print",
+    "$": "variable",
+    "function": "function",
+    "class": "class",
+    "echo": "print-output",
     "<?php": "php-opening-tag",
     "?>": "php-closing-tag",
-    ".": "concat",
-    "(": "open_bracket",
-    ")": "closing_bracket",
-    "{": "opening_curly_bracket",
-    "}": "closing_curly_bracket",
+    ".": "concate",
+    "(": "bracket-opening",
+    ")": "bracket-closing",
+    "{": "curly-bracket-opening",
+    "}": "curly-bracket-closing",
     "#": "single-comment",
     "/*": "multi-line-comment",
     "*/": "multi-line-comment-closing",
@@ -42,41 +42,105 @@ class Token:
         return f"{self.row},{self.col},{self.token},{self.value}"
 
 
-# File reading
-lines = []
-with open("input1.php") as f:
-    for line in f:
-        lines.append(line.strip())
+def get_variable(row, col, line, result_tokens, filename):
+    length = 0
+    while (line[col + length] in (string.digits + string.ascii_letters + "_")):
+        if (length == 1) and (line[col + length] in string.digits):
+            raise Exception(f"{filename}:{row + 1}:{col + length + 1}:VARIABLE NAME CANNOT START WITH DIGIT")
+        else:
+            length += 1
+    result_tokens.append([row + 1, col + 1, "type-identifier", line[col:col + length]])
+    return length
 
-# Going through each line in the file
-result_tokens = []
-for row, line in enumerate(lines):
-    # 5 + 2
-    #     ^
-    col = 0
-    while (col < len(line)):
-        if (line[col] in ".;(){}*-/+$="):
-            result_tokens.append([row + 1, col + 1, TOKENS[line[col]]])
-            col += 1
-        elif ((col + len("<?php")) <= len(line)) and (line[col:col + len("<?php")] == "<?php"):
-            result_tokens.append([row + 1, col + 1, TOKENS["<?php"]])
-            col += len("<?php")
-        elif (col + len("function") <= len(line)) and (line[col:col + len("function")] == "function"):
-            result_tokens.append([row + 1, col + 1, TOKENS["function"]])
-            # function name and etc (WIP)
-        elif (col + len("class") <= len(line)) and (line[col: col + len("class")] == "class"):
-            result_tokens.append([row + 1, col + 1, TOKENS["class"]])
-        elif (col + len("echo") <= len(line)) and (line[col: col + len("echo")] == "echo"):
-            result_tokens.append([row + 1, col + 1, TOKENS["echo"]])
-        elif (col + len("?>") <= len(line)) and (line[col: col + len("?>")]) == "?>":
-            result_tokens.append([row + 1, col + 1, TOKENS["?>"]])
-            col += len("?>")
+
+def token_lexer(filename: str) -> list:
+    lines = []
+    with open(filename) as f:
+        for line in f:
+            lines.append(line)
+
+    result_tokens = []
+    # Going through each line in the file
+    for row, line in enumerate(lines):
+        col = 0
+        while (col < len(line)):
+            # parsing single line comments
+            if ((col + len("//")) <= len(line)) and (line[col:col + len("//")] == "//"):
+                # TODO: Implement logic
+                break
+
+            # taking in single character tokens
+            elif (line[col] in ".;(){}*-/+="):
+                result_tokens.append([row + 1, col + 1, TOKENS[line[col]]])
+                col += 1
+
+            # ignoring whitespace
+            elif (line[col] in string.whitespace):
+                col += 1
+
+            # parsing opening php tag
+            elif ((col + len("<?php")) <= len(line)) and (line[col:col + len("<?php")] == "<?php"):
+                result_tokens.append([row + 1, col + 1, TOKENS["<?php"]])
+                col += len("<?php")
+
+            # parsing function keyword
+            elif (col + len("function") <= len(line)) and (line[col:col + len("function")] == "function"):
+                result_tokens.append([row + 1, col + 1, TOKENS["function"]])
+                col += len("function") + 1
+                col += get_variable(row, col, line, result_tokens, filename)
+
+            # NEED TO TEST
+            # parsing class keyword
+            elif (col + len("class") <= len(line)) and (line[col: col + len("class")] == "class"):
+                result_tokens.append([row + 1, col + 1, TOKENS["class"]])
+                col += len("class") + 1 # add the extra space
+                col += get_variable(row, col, line, result_tokens, filename)
+
+            # parsing echo keyword
+            elif (col + len("echo") <= len(line)) and (line[col: col + len("echo")] == "echo"):
+                result_tokens.append([row + 1, col + 1, TOKENS["echo"]])
+                col += len("echo")
+
+            # parsing closing php tag
+            elif (col + len("?>") <= len(line)) and (line[col: col + len("?>")]) == "?>":
+                result_tokens.append([row + 1, col + 1, TOKENS["?>"]])
+                col += len("?>")
+
+            # parsing integers not decimals
+            elif (line[col] in "0123456789"):
+                length = 0
+                while (line[col + length] in "0123456789"):
+                    length += 1
+                result_tokens.append([row + 1, col + 1, "number", int(line[col: col + length])])
+                col += length
+
+            # parsing variable & identifiers
+            elif line[col] == '$':
+                result_tokens.append([row + 1, col + 1, TOKENS['$']])
+                col += 1
+                col += get_variable(row, col, line, result_tokens, filename)
+
+            # unidentifiable token exception
+            else:
+                raise Exception(f"{filename}:{row + 1}:{col + 1}:INVALID TOKEN FOUND")
+
+    return result_tokens
 
 
 # Final output
-def print_output():
-    return
-    # return "f{line_no},{column_no},{token_class}"
+def print_output(result: list) -> str:
+    for line in result:
+        print(str(line).replace("'", "").replace(" ", "").strip("[]"))
+
+
+def main():
+    filename = input("Enter the PHP filename: ")
+    if filename.endswith(".php"):
+        result_tokens = token_lexer(filename)
+        print_output(result_tokens)
+    else:
+        print("Not a valid PHP source code filename!")
+
 
 if __name__ == "__main__":
-    print(result_tokens)
+    main()
